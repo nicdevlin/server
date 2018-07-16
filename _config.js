757edac2922956e8cd2e8b7307391d7aba9541e6
Nic Devlin <nic@nicdevlin.com.au>
2018-07-16 13:27:10 +1000

    removing files

--- _config.js
+++
@@ -1,8 +0,0 @@
-
-const config = {}
-
-config.mongoURI = {
-    development: 'mongodb://admin:admin123@ds235411.mlab.com:35411/ca-final-assignment-db',
-    test: 'mongodb://localhost/assignment-test-server'
-}
-module.exports = config
